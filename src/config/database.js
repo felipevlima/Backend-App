d5b55be47e50a9aef197052e9a613ca98498678a
Felipe Vieira <felip.3lima@hotmail.com>
2019-12-11 15:04:51 -0300

    chore: Sucrase Add

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'teste123',
+  database: 'dashplan',
   define: {
     timestamps: true,
     underscored: true,
